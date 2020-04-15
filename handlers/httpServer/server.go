26457467a0197fb1af1fa0560f6b84c62b29758b
j-ew-s <gabriel.scavassa@outlook.com>
2020-04-15 12:35:27 +0100

    Fixing names on server.go

--- handlers/httpServer/server.go
+++ handlers/httpServer/server.go
@@ -2,8 +2,8 @@ package httpserverhandler
 
 import (
 	"github.com/buaazp/fasthttprouter"
-	receiptscontroller "github.com/j-ew-s/receipts-api/api"
-	"github.com/j-ew-s/receipts-api/configs"
+	recipescontroller "github.com/j-ew-s/recipes-api/api"
+	"github.com/j-ew-s/recipes-api/configs"
 	"github.com/valyala/fasthttp"
 )
 
@@ -26,12 +26,12 @@ func CreateHTTPServer() *fasthttp.Server {
 // Configure all routes for controllers
 func setRoutes(router *fasthttprouter.Router) {
 
-	router.GET("/ping/", receiptscontroller.Ping)
-	router.POST("/receipts/", receiptscontroller.Create)
-	router.GET("/receipts/", receiptscontroller.Get)
-	router.GET("/receipts/tags/", receiptscontroller.GetByTags)
-	router.GET("/receipts/id/:id", receiptscontroller.GetByID)
-	router.PUT("/receipts/:id", receiptscontroller.Put)
-	router.DELETE("/receipts/:id", receiptscontroller.Delete)
-	/*router.GET("/receipts/terms/:search", controller.Search)*/
+	router.GET("/ping/", recipescontroller.Ping)
+	router.POST("/recipes/", recipescontroller.Create)
+	router.GET("/recipes/", recipescontroller.Get)
+	router.GET("/recipes/tags/", recipescontroller.GetByTags)
+	router.GET("/recipes/id/:id", recipescontroller.GetByID)
+	router.PUT("/recipes/:id", recipescontroller.Put)
+	router.DELETE("/recipes/:id", recipescontroller.Delete)
+	/*router.GET("/recipes/terms/:search", controller.Search)*/
 }
